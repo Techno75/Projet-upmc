7696ac9c74b03cb038c58c3378bee3107a3eb653
Techno75 <gdepretz@gmail.com>
2019-06-18 14:12:17 +0200

    Avancement de la page Match

--- src/Components/Match/ListView.jsx
+++ src/Components/Match/ListView.jsx
@@ -19,32 +19,49 @@ class ListView extends Component {
       return false;
     })
 
+    const matchInProgressList = this.props.matchDataList.filter((match)=>{
+      if(match.status === "in progress"){
+        return match
+      }
+      return false;
+    })
+
     return (
           <div>
             <h1>Calendar</h1>
             <p>Match already played</p>
-            {matchAlreadyPlayedList.map((match)=>{
+            {matchAlreadyPlayedList.map((match, index)=>{
               return(
-                <div className="match-card-row">
+                <div className="match-card-row" key={index}>
                    <div className="country-card">
                       <img src={require("./../../Assets/Images/Flags/" + match.home_team.code + ".jpg")} alt="flag"/>
-                      {match.home_team_country}
+                      <p>{match.home_team_country}</p>
                    </div>
                   <div className="match-card-row-score">
-                      {match.home_team.goals} - {match.away_team.goals}
+                      <p>{match.home_team.goals} - {match.away_team.goals}</p>
                    </div>
                    <div className="country-card">
-                      {match.away_team_country}
+                      <p>{match.away_team_country}</p>
                       <img src={require("./../../Assets/Images/Flags/" + match.away_team.code + ".jpg")} alt="flag"/>
                    </div>
-
                 </div>
               )
             })}
-            {matchNotPlayedList.map((match)=>{
+            {matchNotPlayedList.map((match, index)=>{
               return(
-                <div>
-                   {moment(match.datetime).format('MM-DD-YYYY hh:mm a')} | {match.home_team_country} vs {match.away_team_country}
+                <div className="match-card-row" key={index}>
+                   <div className="country-card">
+                    <img src={require("./../../Assets/Images/Flags/" + match.home_team.code + ".jpg")} alt="flag"/>
+                      <p>{match.home_team_country}</p>
+                   </div>
+                  <div className="match-card-row-date">
+                      <p>{moment(match.datetime).format('MM/DD/YYYY')}</p>
+                      <p>{moment(match.datetime).format('hh:mm a')}</p>
+                   </div>
+                   <div className="country-card">
+                      <p>{match.away_team_country}</p>
+                      <img src={require("./../../Assets/Images/Flags/" + match.away_team.code + ".jpg")} alt="flag"/>
+                   </div>
                 </div>
               )
             })}
