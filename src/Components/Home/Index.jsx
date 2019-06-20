7565f66b6ecf2ea334a38667c0a2a2e89b8e33d7
RobinPalmier <robinpalmier98@gmail.com>
2019-06-20 12:31:41 +0200

    Home

--- src/Components/Home/Index.jsx
+++ src/Components/Home/Index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import homeImage from './../../Assets/Images/home.jpg';
 
 class Home extends Component {
 
+
+
   render() {
     return (
-          <div>
-            <h1>Home toto</h1>
+          <div className="content-home">
+            <div className="field">
+              <img src={homeImage} alt="home-footfem-img" />
+              <h1>FootFem supports women's soccer !</h1>
+            </div>
+            <div className="content-home-actu">
+              <h2>News</h2>
+            </div>
           </div>
     )
   }
