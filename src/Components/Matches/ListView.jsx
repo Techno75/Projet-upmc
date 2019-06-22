a50e3bfa98556cd20c9cac6ef45b0fac8c3acdd6
Techno75 <gdepretz@gmail.com>
2019-06-22 17:07:37 +0200

    Affichage dans la page matches, en vue list, des nom des différentes phase (quart, demie, final)

--- src/Components/Matches/ListView.jsx
+++ src/Components/Matches/ListView.jsx
@@ -52,22 +52,25 @@ class ListView extends Component {
             <h1>Calendar</h1>
             {matchAlreadyPlayedList.map((match, index)=>{
               return(
-                <Link to={{pathname : routesList[10].path, aboutProps : {match : match}}} key={index}>
-                  <div className="match-card-row" >
-                     <div className="country-card">
-                        <img src={require("./../../Assets/Images/Flags/" + match.home_team.code + ".jpg")} alt="flag"/>
-                        <p>{match.home_team_country}</p>
-                     </div>
-                    <div className="match-card-row-score">
-                        <p>{moment(match.datetime).format('MM-DD-YYYY')}</p>
-                        <p>{match.home_team.goals} - {match.away_team.goals}</p>
-                     </div>
-                     <div className="country-card">
-                        <p>{match.away_team_country}</p>
-                        <img src={require("./../../Assets/Images/Flags/" + match.away_team.code + ".jpg")} alt="flag"/>
-                     </div>
-                  </div>
-                </Link>
+                <div key={index}>
+                  {index === 36 || index === 44 || index === 48 || index === 50 || index === 51 ? <h1>{match.stage_name}</h1> : ""}
+                  <Link to={{pathname : routesList[10].path, aboutProps : {match : match}}} >
+                    <div className="match-card-row" >
+                       <div className="country-card">
+                          <img src={require("./../../Assets/Images/Flags/" + match.home_team.code + ".jpg")} alt="flag"/>
+                          <p>{match.home_team_country}</p>
+                       </div>
+                      <div className="match-card-row-score">
+                          <p>{moment(match.datetime).format('MM-DD-YYYY')}</p>
+                          <p>{match.home_team.goals} - {match.away_team.goals}</p>
+                       </div>
+                       <div className="country-card">
+                          <p>{match.away_team_country}</p>
+                          <img src={require("./../../Assets/Images/Flags/" + match.away_team.code + ".jpg")} alt="flag"/>
+                       </div>
+                    </div>
+                  </Link>
+                </div>
               )
             })}
             {matchNotPlayedList.map((match, index)=>{
