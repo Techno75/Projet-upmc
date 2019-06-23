f3d7c22a4c6325b54d3ec0890356d4d5a1aaf136
Techno75 <gdepretz@gmail.com>
2019-06-23 18:17:31 +0200

    page team podif

--- src/Components/Team/Index.jsx
+++ src/Components/Team/Index.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Team extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      team : {},
+    };
+  }
+
+
+  componentDidMount(){
+    this.setState({team : this.props.location.aboutProps.team});
+  }
+
   render() {
+    const team = this.state.team;
     return (
           <div>
-            <h1>Team</h1>
+            <h1>{team.country}</h1>
           </div>
     )
   }
