58a9ab5eda7b189dcab557d6d24f7e213f3faf7b
Techno75 <gdepretz@gmail.com>
2019-06-23 14:55:44 +0200

    Ajout du boutton follow match

--- src/Components/Groups/Index.jsx
+++ src/Components/Groups/Index.jsx
@@ -7,20 +7,35 @@ class Groups extends Component {
     super(props)
     this.state = {
       groupList : [],
-      groupToDisplay : 0,
+      groupIdToDisplay : 2,
+      groupeTodisplay : {letter : "", ordered_teams:[{country : "", games_played : 0, draws : 0, }]}
     }
   }
   componentDidMount(){
     fetchDataToApi(API_ROUTE + 'teams/group_results', 'GET')
     .then((groupList)=>{
-      this.setState({groupList})
+      this.setState({groupList,})
     });
   }
 
   render() {
+    let group = {ordered_teams:[]};
+    group = this.state.groupList[this.state.groupIdToDisplay];
+
+
     return (
           <div>
             <h1>Groupes</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Country</th>
+                </tr>
+              </thead>
+              <tbody>
+
+              </tbody>
+            </table>
           </div>
     )
   }
--- src/Components/Matches/ListView.jsx
+++ src/Components/Matches/ListView.jsx
@@ -75,23 +75,26 @@ class ListView extends Component {
             })}
             {matchNotPlayedList.map((match, index)=>{
               return(
-                <Link to={{pathname : routesList[10].path, aboutProps : {match : match}}}  key={index}>
-                  <div className="match-card-row">
-                     <div className="country-card">
-                      <img src={require("./../../Assets/Images/Flags/" + match.home_team.code + ".jpg")} alt="flag"/>
-                        <p>{match.home_team_country}</p>
-                     </div>
-                    <div className="match-card-row-date">
-                        <p>{moment(match.datetime).format('MM-DD-YYYY')}</p>
-                        <p>{moment(match.datetime).format('hh:mm a')}</p>
-                     </div>
-                     <div className="country-card">
-                        <p>{match.away_team_country}</p>
-                        <img src={require("./../../Assets/Images/Flags/" + match.away_team.code + ".jpg")} alt="flag"/>
-                     </div>
+                <div className="match-card-row-with-button">
+                  <Link to={{pathname : routesList[10].path, aboutProps : {match : match}}}  key={index}>
+                    <div className="match-card-row">
+                       <div className="country-card">
+                        <img src={require("./../../Assets/Images/Flags/" + match.home_team.code + ".jpg")} alt="flag"/>
+                          <p>{match.home_team_country}</p>
+                       </div>
+                      <div className="match-card-row-date">
+                          <p>{moment(match.datetime).format('MM-DD-YYYY')}</p>
+                          <p>{moment(match.datetime).format('hh:mm a')}</p>
+                       </div>
+                       <div className="country-card">
+                          <p>{match.away_team_country}</p>
+                          <img src={require("./../../Assets/Images/Flags/" + match.away_team.code + ".jpg")} alt="flag"/>
+                       </div>
 
-                  </div>
-                </Link>
+                    </div>
+                  </Link>
+                  <button className="follow-match-button">follow</button>
+                </div>
               )
             })}
           </div>
