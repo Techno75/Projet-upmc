0b25a81a0d10e7853e6a4be06e868ea088c87650
Techno75 <gdepretz@gmail.com>
2019-06-24 13:23:43 +0200

    test

--- src/Components/Team/Index.jsx
+++ src/Components/Team/Index.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { fetchDataToApi } from './../../Functions/FetchToApi.js';
+import { API_ROUTE } from './../../Constantes/ApiRoute.js';
 
 class Team extends Component {
   constructor(props){
     super(props);
     this.state = {
       team : {},
+
     };
   }
 
 
-  componentDidMount(){
+  componentWillMount(){
     this.setState({team : this.props.location.aboutProps.team});
+    fetchDataToApi(API_ROUTE + "matches/country?fifa_code=" + this.props.location.aboutProps.team.fifa_code)
+    .then((matchTeam)=>{
+      console.log(matchTeam);
+    })
   }
 
   render() {
@@ -18,6 +25,7 @@ class Team extends Component {
     return (
           <div>
             <h1>{team.country}</h1>
+            
           </div>
     )
   }
