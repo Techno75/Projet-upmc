ba1537339f91614a8a77b6a4d2b5e3247849b269
RobinPalmier <robinpalmier98@gmail.com>
2019-06-24 02:07:49 +0200

    Page groupe fini !

--- src/Components/Groups/Index.jsx
+++ src/Components/Groups/Index.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { fetchDataToApi } from './../../Functions/FetchToApi.js';
 import { API_ROUTE } from './../../Constantes/ApiRoute.js';
 import { Link } from 'react-router-dom';
-import {routesList} from '../../Constantes/Routes.js';
+import { routesList } from '../../Constantes/Routes.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 class Groups extends Component {
   constructor(props){
@@ -33,24 +35,24 @@ class Groups extends Component {
   render() {
     return (
           <div className="content-groups">
+            <h1 className="rsp-table-group">Group {this.state.groupeTodisplay.letter}</h1>
             <table>
               <thead>
                 <tr>
                   <th>Group {this.state.groupeTodisplay.letter}</th>
-                  <th>Rank</th>
-                  <th>Country</th>
                   <th>Games Played</th>
                   <th>Win</th>
-                  <th>Draw</th>
                   <th>Loose</th>
                   <th>Goal For</th>
                   <th>Goal Against</th>
-                  <th>+/-</th>
+                  <th>Goals Differential</th>
+                  <th>Draws</th>
                   <th>Points</th>
                 </tr>
               </thead>
               <tbody>
                 {this.state.groupeTodisplay.ordered_teams.map((team, index)=>{
+                  console.log(team);
                   return(
                       <tr key={index}>
                         <td>
@@ -59,38 +61,46 @@ class Groups extends Component {
                             </Link>
                         </td>
                         <td>
+                          <p className="rsp-table-group">Games Played</p>
                           {team.games_played}
                         </td>
                         <td>
+                          <p className="rsp-table-group">Win</p>
                           {team.wins}
                         </td>
                         <td>
-                          {team.draws}
-                        </td>
-                        <td>
+                          <p className="rsp-table-group">Loose</p>
                           {team.losses}
                         </td>
                         <td>
+                          <p className="rsp-table-group">Goals For</p>
                           {team.goals_for}
                         </td>
                         <td>
+                          <p className="rsp-table-group">Goals Against</p>
                           {team.goals_against}
                         </td>
                         <td>
+                        <p className="rsp-table-group">Goals Differential</p>
                           {team.goal_differential}
                         </td>
                         <td>
+                          <p className="rsp-table-group">Draws</p>
+                          {team.draws}
+                        </td>
+                        <td>
+                          <p className="rsp-table-group">Points</p>
                           {team.points}
                         </td>
-                        <td></td>
-                        <td></td>
                       </tr>
                   )
                 })}
               </tbody>
             </table>
-            {this.state.groupIdToDisplay > 0 ? <button onClick={this.goPrevious.bind(this)}>Previous</button> : ""}
-            {this.state.groupIdToDisplay < 5 ? <button onClick={this.goNext.bind(this)}>Next</button> : ""}
+            <div className='content-button'>
+              {this.state.groupIdToDisplay > 0 ? <button onClick={this.goPrevious.bind(this)} className='prev-button-group'><FontAwesomeIcon icon={faArrowLeft} className='picto-groups'/> Previous</button> : ""}
+              {this.state.groupIdToDisplay < 5 ? <button onClick={this.goNext.bind(this)} className='next-button-group'>Next <FontAwesomeIcon icon={faArrowRight} className='picto-groups'/> </button> : ""}
+            </div>
           </div>
     )
   }
