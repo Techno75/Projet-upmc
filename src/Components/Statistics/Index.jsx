f0ccf705ff175b88428df50810ebb7f6f469b977
Techno75 <gdepretz@gmail.com>
2019-06-25 12:48:46 +0200

    statistics

--- src/Components/Statistics/Index.jsx
+++ src/Components/Statistics/Index.jsx
@@ -9,7 +9,7 @@ class Statistics extends Component {
       scorerList : [],
       numberOfMatchPlayed : 0,
       numberOfGoalScored : 0,
-      goalList : []
+      goalList : [],
       }
     }
 
@@ -17,29 +17,78 @@ class Statistics extends Component {
       fetchDataToApi(API_ROUTE + "matches")
       .then((matchList)=>{
 
-          const allMatchAlreadyPlayed = matchList.filter((match)=>{if (match.status === "completed"){return match;}})
-          const homeEventList = allMatchAlreadyPlayed.map((match)=>match.home_team_events);
-          const awayEventList = allMatchAlreadyPlayed.map((match)=>match.away_team_events);
+          const allMatchAlreadyPlayed = matchList.filter((match)=>{if (match.status === "completed"){return match;}});
+
+          const allMatchOfHomeTeamAlreadyPlayedReformatList = allMatchAlreadyPlayed.map((match)=>{
+            const finalObjectFormatList = match.home_team_events.map((event)=>{
+               event.country = match.home_team_country;
+               return event;
+            })
+            return finalObjectFormatList;
+          })
+
+          const allMatchOfAwayTeamAlreadyPlayedReformatList = allMatchAlreadyPlayed.map((match)=>{
+            const finalObjectFormatList = match.away_team_events.map((event)=>{
+               event.country = match.away_team_country;
+               return event;
+            })
+            return finalObjectFormatList;
+          })
+
           const allEventList = [];
 
-          homeEventList.forEach((evtList)=>{allEventList.push(...evtList);})
-          awayEventList.forEach((evtList)=>{allEventList.push(...evtList);})
+          allMatchOfHomeTeamAlreadyPlayedReformatList.forEach((evtList)=>{allEventList.push(...evtList);})
+          allMatchOfAwayTeamAlreadyPlayedReformatList.forEach((evtList)=>{allEventList.push(...evtList);})
 
           const allGoals = allEventList.filter((evt)=>{if (evt.type_of_event === "goal" || evt.type_of_event === "goal-penalty") {return evt}})
 
-          this.setState({numberOfMatchPlayed : allMatchAlreadyPlayed.length, numberOfGoalScored : allGoals.length, goalList : allGoals})
+          const scorerNameListToCheck = [];
+          const scorerNameListTmp = allGoals.filter((goal)=>{
+            if (scorerNameListToCheck.indexOf(goal.player) === -1) {
+              scorerNameListToCheck.push(goal.player);
+              return goal
+            }
+          })
+          const scorerList = scorerNameListTmp.map((goal)=>{
+            return {name : goal.player, country : goal.country, goalList: []};
+          })
+
+          allGoals.forEach((goal)=>{
+            scorerList.forEach((scorer)=>{
+              if (goal.player === scorer.name) {
+                scorer.goalList.push(goal);
+              }
+            })
+          })
+
+          this.setState({scorerList : scorerList, numberOfMatchPlayed : allMatchAlreadyPlayed.length, numberOfGoalScored : allGoals.length, goalList : allGoals})
       })
     }
 
   render() {
-    console.log(this.state.goalList);
     const goalPerMatch = Math.round((this.state.numberOfGoalScored / this.state.numberOfMatchPlayed) * 10) / 10;
+
     return (
           <div>
             <h1>Statistics</h1>
-            <h2>Number of Match played : {this.state.numberOfMatchPlayed}</h2>
-            <h2>Number of goals scored : {this.state.numberOfGoalScored}</h2>
-            <h2>Goal per match : {goalPerMatch}</h2>
+            <div>
+              {
+                this.state.scorerList.map((scorer)=>{
+                  return(
+                    <div>
+                      <p>{scorer.name}</p>
+                      <p>{scorer.country}</p>
+                      <p>{scorer.goalList.length}</p>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div>
+              <h2>Number of Match played : {this.state.numberOfMatchPlayed}</h2>
+              <h2>Number of goals scored : {this.state.numberOfGoalScored}</h2>
+              <h2>Goal per match : {goalPerMatch}</h2>
+            </div>
           </div>
     )
   }
