42fb4713f28b8f1cb84e7384e94b4ecc67be06fd
mueenou <mueen.hossain@gmail.com>
2019-06-26 17:33:41 +0200

    Administration done

--- src/Components/AdministrativePanel/UserUpdate.jsx
+++ src/Components/AdministrativePanel/UserUpdate.jsx
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import { fetchDataToApi } from './../../Functions/FetchToApi.js';
+import {REST_ROUTE} from "../../Constantes/ApiRoute";
 
 class UserUpdate extends Component {
 
+modifAdmin(id, admin){
+  if(admin){
+    admin = 0;
+  }else{
+    admin = 1
+  }
+
+  fetchDataToApi(REST_ROUTE + 'users/setAdmin/' + id, 'PUT', {isAdmin: admin})
+  .then((response)=>{
+    this.props.getAllUsers();
+    console.log('success update');
+  })
+
+}
 
   render() {
+    console.log(this.props.listUsers);
     return(
-      <h2>User Update</h2>
+      <div className='wrapper-listComments'>
+        <table>
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>LastName</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>isAdmin</th>
+            </tr>
+          </thead>
+        {
+          this.props.listUsers.map((users, index)=>{
+            console.log(users.isAdmin);
+            return(
+                <tbody>
+                  <tr>
+                    <td><p className='rsp-table-admin'>Firstname</p>{users.firstname}</td>
+                    <td><p className='rsp-table-admin'>LastName</p>{users.lastname}</td>
+                    <td><p className='rsp-table-admin'>Username</p>{users.username}</td>
+                    <td><p className='rsp-table-admin'>Email</p>{users.email}</td>
+                    <td><p className='rsp-table-admin'>isAdmin</p>
+                      <button onClick={this.modifAdmin.bind(this, users.id, users.isAdmin)}>
+                        <div className={users.isAdmin === false ? "toggle-container" : "toggle-container contenaire-button-admin"}>
+                          <div className={users.isAdmin === false ? "toggle-cursor" : "toggle-cursor button-admin"}>
+                          </div>
+                        </div>
+                      </button>
+                  </td>
+                  </tr>
+                </tbody>
+            )
+          })
+        }
+        </table>
+      </div>
     )
   }
 }
